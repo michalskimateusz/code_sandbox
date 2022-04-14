40fac0107cf264f0c8c8c9434f21c6a900283c2c
michalskimateusz <michalskimateusz@outlook.com>
2022-04-14 21:23:26 +0200

    iframe security setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
-import ReactDOM from "react-dom";
 import * as esbuild from "esbuild-wasm";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -24,6 +23,9 @@ const App = () => {
     if (!ref.current) {
       return;
     }
+
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -35,19 +37,42 @@ const App = () => {
       },
     });
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+  <html>
+  <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data)
+            } catch (error) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime error</h4>' + error + '</div>';
+              console.error(error);
+            }
+          }, false)
+      </script>
+    </body>
+  </html>
+  `;
 
   return (
     <div>
-      <textarea
-        onChange={(e) => setInput(e.target.value)}
-        value={input}
-      ></textarea>
+      <textarea onChange={(e) => setInput(e.target.value)} value={input} />
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        title="preview"
+        ref={iframe}
+        srcDoc={html}
+        sandbox="allow-scripts"
+      />
     </div>
   );
 };
